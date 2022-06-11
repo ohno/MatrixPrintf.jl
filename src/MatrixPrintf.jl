b0d3317a8c8da7d88328effad5a878c3bcb10eb6
shuhei-ohno <shuheiohno1414@gmail.com>
2022-06-12 01:04:03 +0900

    first commit

--- src/MatrixPrintf.jl
+++ src/MatrixPrintf.jl
@@ -1,5 +1,47 @@
 module MatrixPrintf
 
-# Write your package code here.
+  using Printf
+
+  function integer_digit(x)
+    return x<1 ? 1 : Int64(floor(log10(x))+1)
+  end
+
+  function auto_format(M, digit)
+    if sum(M .< 0)==0 && digit==0
+      margin = 0
+    elseif sum(M .< 0)==0 && digit>0
+      margin = 1
+    elseif sum(M .< 0)!=0 && digit==0
+      margin = 1
+    else
+      margin = 2
+    end
+    return "%$(maximum(integer_digit.(abs.(M))) + digit + margin).$(digit)f"
+  end
+
+  function msprintf(M; digit=1, format=:auto, symbol="")
+    # Auto Format
+    if format==:auto
+      format = auto_format(M, digit)
+    end
+    # Convert each element of matrix M to formed string martix FSM
+    eval_sprintf(x) = eval(Meta.parse("@sprintf(\"$(format)\", $x)"))
+    FSM = eval_sprintf.(M)
+    # Output FSM
+    if symbol==""
+      str = "$FSM"
+      str = replace(str, "\""=>"")
+      str = replace(str, ";"=>";\n")
+    else
+      str = "$symbol = $FSM"            
+      str = replace(str, "\""=>"")
+      str = replace(str, ";"=>";\n"*" "^(length(symbol)+3))
+    end
+    return str
+  end
+
+  function mprintf(M; args...)
+    println(msprintf(M; args...))
+  end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,38 @@ using MatrixPrintf
 using Test
 
 @testset "MatrixPrintf.jl" begin
-    # Write your tests here.
+  A = [1 2;
+      3 4]
+  B = [1  0;
+      0 -1]
+  C = [0.01   2;
+      3.0 -400]
+
+  @test MatrixPrintf.msprintf(A) == "[1.0 2.0;\n 3.0 4.0]"
+  @test MatrixPrintf.msprintf(B) == "[ 1.0  0.0;\n  0.0 -1.0]"
+  @test MatrixPrintf.msprintf(C) == "[   0.0    2.0;\n    3.0 -400.0]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A") == "A = [1.0 2.0;\n     3.0 4.0]"
+  @test MatrixPrintf.msprintf(B, symbol="B") == "B = [ 1.0  0.0;\n      0.0 -1.0]"
+  @test MatrixPrintf.msprintf(C, symbol="C") == "C = [   0.0    2.0;\n        3.0 -400.0]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A", digit=5) == "A = [1.00000 2.00000;\n     3.00000 4.00000]"
+  @test MatrixPrintf.msprintf(B, symbol="B", digit=5) == "B = [ 1.00000  0.00000;\n      0.00000 -1.00000]"
+  @test MatrixPrintf.msprintf(C, symbol="C", digit=5) == "C = [   0.01000    2.00000;\n        3.00000 -400.00000]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A", digit=2) == "A = [1.00 2.00;\n     3.00 4.00]"
+  @test MatrixPrintf.msprintf(B, symbol="B", digit=2) == "B = [ 1.00  0.00;\n      0.00 -1.00]"
+  @test MatrixPrintf.msprintf(C, symbol="C", digit=2) == "C = [   0.01    2.00;\n        3.00 -400.00]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A", digit=0) == "A = [1 2;\n     3 4]"
+  @test MatrixPrintf.msprintf(B, symbol="B", digit=0) == "B = [ 1  0;\n      0 -1]"
+  @test MatrixPrintf.msprintf(C, symbol="C", digit=0) == "C = [   0    2;\n        3 -400]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A", format="%d") == "A = [1 2;\n     3 4]"
+  @test MatrixPrintf.msprintf(B, symbol="B", format="%d") == "B = [1 0;\n     0 -1]"
+  @test MatrixPrintf.msprintf(C, symbol="C", format="%d") == "C = [0 2;\n     3 -400]"
+
+  @test MatrixPrintf.msprintf(A, symbol="A", format="%8.1e") == "A = [ 1.0e+00  2.0e+00;\n      3.0e+00  4.0e+00]"
+  @test MatrixPrintf.msprintf(B, symbol="B", format="%8.1e") == "B = [ 1.0e+00  0.0e+00;\n      0.0e+00 -1.0e+00]"
+  @test MatrixPrintf.msprintf(C, symbol="C", format="%8.1e") == "C = [ 1.0e-02  2.0e+00;\n      3.0e+00 -4.0e+02]"
 end
